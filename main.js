b57cf995e06f8ba1e8aee0088c9d4e5f62d0e6a0
Vũ Văn Dũng <daisyss159@gmail.com>
2023-02-24 11:58:25 +0700

    commit

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-alert('hello');
+const menu = document.querySelector('.menu');
+const modal = document.querySelector('.model');
+const modalClose = document.querySelector('.menu_ds-close')
+
+function ShowMenu() {
+    modal.classList.add('open')
+}
+
+function RemoveMenu() {
+    modal.classList.remove('open')
+}
+
+menu.addEventListener('click', ShowMenu);
+modalClose.addEventListener('click', RemoveMenu)
+modal.addEventListener('click', RemoveMenu);
