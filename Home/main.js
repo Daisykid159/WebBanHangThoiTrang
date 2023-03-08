b412987cf3ae5d264c414c0e8d32f8c0cc556d0d
Vũ Văn Dũng <daisyss159@gmail.com>
2023-03-08 22:36:14 +0700

    commit

--- Home/main.js
+++ Home/main.js
@@ -1,1 +1,15 @@
-alert('Welcome to website of Team 3');
+// alert('Welcome to website of Team 3');
+const prd = document.querySelector('#product');
+const prdLi = document.querySelector('.header_product');
+
+function active() {
+    prd.classList.remove('none');
+    prd.classList.add('active')
+}
+
+function removeActive() {
+    prd.classList.remove('active');
+    prd.classList.add('none')
+}
+
+prdLi.addEventListener('blur', active)
